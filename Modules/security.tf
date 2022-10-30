1c5e643b3dad114f3f6919fad43fbbd752802279
Jagan Rajagopal <jaganrajagopal@outlook.com>
2022-10-30 14:22:52 +0530

    adding the changes teamaccount A

--- Modules/security.tf
+++ Modules/security.tf
@@ -1,10 +1,25 @@
-resource "aws_security_group" "securitygroup_ec2_nginx" {
-  name = "terraform-example-instance"
-  
+resource "aws_security_group" "ec2_sg_group" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
   ingress {
-    from_port	  = 8080
-    to_port	    = 8080
-    protocol	  = "tcp"
-    cidr_blocks	= ["0.0.0.0/0"]
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Kering Security group"
   }
 }
--- TeamAccountA/main.tf
+++ TeamAccountA/main.tf
@@ -1,18 +1,45 @@
 provider "aws" {
   region  = "${var.region}"
-  version = "~> 2.0"
+  #version = "~> 4.0"
 }
 # Creation of Ec2 instance (Reusing the module )
 module "e2instanceModule" {
-    source = "../Modules/ec2"
-    
-  
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami                    = var.ami
+  instance_type          = "t2.micro"
+  #key_name               = "user1"
+  monitoring             = true
+  #vpc_security_group_ids = ["sg-12345678"]
+  #subnet_id              = data.aws_subnets.private
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
-module "aws_vpc" {
-    source = "../Modules/vpc"
-  
-}
-module "securitygroup_ec2_nginx" {
-    source = "../Modules/security"
-  
+
+
+
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  #vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 }
